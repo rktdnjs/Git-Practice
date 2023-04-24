808be57ab9f5e000fe0575b9f846a88010058ac0
rktdnjs <rktdnjs@naver.com>
2023-04-24 21:35:16 +0900

    feat : Modify fizzbuzz.py
    modify fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,11 @@
 # fizz buzz fizzbuzz 셋 중 하나를 출력하는 함수
 # 3 : fizz / 5 : buzz / 15 : fizzbuzz
-def do_fizzbuzz():
-    print('Hello')
+def do_fizzbuzz(num):
+    for i in range(1, num + 1):
+        if i%3 == 0:
+            print('fizz')
+        else:
+            print(f'{i}')
 
 if __name__ == '__main__':
-    do_fizzbuzz()
+    do_fizzbuzz(16)
